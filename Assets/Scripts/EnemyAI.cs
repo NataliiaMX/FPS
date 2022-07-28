e3db91f2e76c3c5bf4d146496bb6fa1e1d66a4fe
Nataliia <natalia.egorova978@gmail.com>
2022-07-28 19:08:06 +0400

    Made enemy face the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,7 +7,9 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
@@ -15,11 +17,16 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
-        SetDestination();
+        ChasePlayer();
     }
 
-    private void SetDestination()
+    private void ChasePlayer()
     {
-        navMeshAgent.SetDestination(target.transform.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
     }
 }
