1907eabb64015d60279ac3d4b90dacb701a8d908
Nataliia <natalia.egorova978@gmail.com>
2022-08-10 21:48:54 +0300

    Implemented a little bit of ammo

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -73,7 +73,6 @@ public class EnemyAI : MonoBehaviour
         // Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         // transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed); 
         // // slerp allows to rotate smoothly between two vectors
-        Debug.Log("Looking at target");
         transform.LookAt(target);
     }
 
--- Assets/Scripts/Wearpon.cs
+++ Assets/Scripts/Wearpon.cs
@@ -10,7 +10,12 @@ public class Wearpon : MonoBehaviour
     [SerializeField] GameObject hitVFX;
     [SerializeField] float range = 30f;
     [SerializeField] float damagePoints = 40f;
-    
+    [SerializeField] Ammo ammoSlot;
+
+    private void Start()
+    {
+        ammoSlot = FindObjectOfType<Ammo>();
+    }
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -21,17 +26,26 @@ public class Wearpon : MonoBehaviour
 
     private void Shoot()
     {
-        gunFireVFX.Play();
-        ProcessRaycast();
+        if (ammoSlot.GetCurrentAmmo() > 0)
+        {
+            ProcessRaycast();
+            ammoSlot.ReduceCurrentAmmo();
+        }
+        else
+        {
+            Debug.Log("out of ammo");
+            return;
+        }
     }
 
     private void ProcessRaycast()
     {
+
         RaycastHit hit;
         if (Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range))
         {
+            gunFireVFX.Play();
             CreateHitImpact(hit);
-            // Debug.Log(hit.transform.name);
             EnemyHealth1 target = hit.transform.GetComponent<EnemyHealth1>();
             target?.TakeDamage(damagePoints); //if target !=null
         }
