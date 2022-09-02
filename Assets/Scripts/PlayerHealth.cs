cafca8d69ce3950f47d2493131ddcca47e8c31b0
Nataliia <natalia.egorova978@gmail.com>
2022-09-02 19:08:44 +0300

    Player HP display added

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float playerHP = 100;
+    [SerializeField] TextMeshProUGUI hpText;
     bool isDead = true;
+
+    private void Update() 
+    {
+        DisplayHP();
+    }
+
+    private void DisplayHP()
+    {
+        hpText.text = playerHP.ToString();
+    }
 
     public void IncreaseHealth (float hpIncrement)
     {
